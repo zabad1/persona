c38d6a4c01a7a84c057d99d3b35cc8bf8d980d6e
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-12 09:34:12 -0700

    Added to paragraph 2 h2.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<!-- Paragraph 1-->
 		<h3>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h3>
 		<!-- Paragraph 2-->
-		<h3>A thing you can do on IMDB is add to the content of the film. So you as a user can add to the triva. If you find imformation or spot something in a movie that isn't on the site, you can add to it. But you must have a IMDB account to do so. </h3>
+		<h3>A thing you can do on IMDB is add to the content of the film. So you as a user can add to the triva. If you find imformation or spot something in a movie that isn't on the site, you can add to it. But you must have a IMDB account to do so. But there can be problems with the input information. Highly popluar movies can have a lot information and lesser known movies will have very little info to their contents. </h3>
 		<!-- Paragraph 3-->
 		<h3>Here is two examples of different movies, one being a highly popular movie that has a lot of triva and the second example having very little triva added to it.</h3>
 		<!-- Links to movie triva-->
