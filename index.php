c135eca4e9d49441749d8094d926a574cde12e9d
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-12 13:47:38 -0700

    updated case, persona, image again

--- index.php
+++ index.php
@@ -4,18 +4,26 @@
 	<title>Zak Abad Persona</title>
 </head>
 	<body>
-		<h3> Name: Zak Abad
+		<h1> Name: Zak Abad
 			<br>
-			<p><img src="Images/IMG_5040.JPG" alt="Drinking my Costco size wine bottle" style="float:right""width: 150px; height: 50px"></p>
+			<img src="Images/IMG_5040.JPG" alt="Drinking my Costco size wine bottle" style="float:right;width:250px; height:350px";>
 		Age: 32
 			<br>
-		Occupation: Cinema Nerd</h3>
+		Occupation: Cinema Nerd</h1>
 		<!-- Persona-->
-		<h4>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h4>
+		<h3>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h3>
 		<!-- Case-->
-		<h4>All though I love reading about triva and other things on IMDB. I need to put a stop to the useless triva information that other users put on the site. I must hack IMDB and become the main host so that no one can put useless information on it.</h4>
+		<h3>All though I love reading about triva and other things on IMDB. I need to put a stop to the useless triva information that other users put on the site. I must hack IMDB and become the main host so that no one can put useless information on it.</h3>
 		<!--Examples of bad triva-->
-		<h4>Here is two examples of different movies, one being a highly popular movie that has a lot of triva and the second example having very little triva added to it.</h4>
+		<h3>Here is two examples of different movies, one being a highly popular movie that has a lot of triva and the second example having very little triva added to it.</h3>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
 		<!-- Links to movie triva-->
 		<img src="Images/madmaxfuryroad.jpg" width="450"; height="250";>
 		<br>
@@ -26,7 +34,7 @@
 		<img src="Images/theAdventuresofPrinceAchmed.jpg" width="450"; height="250">
 		<br>
 		<a href="http://www.imdb.com/title/tt0015532/trivia?ref_=tt_ql_2"> The Adventures of Prince Achmed</a>
-		<!-- Paragraph 3-->
+		<!-- Interaction flow-->
 		<ol>1)Turns on comupter
 			<br>
 			2)Starts hacking Server
