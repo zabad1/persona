e16dcb593c6d0c33622db801694e8c18083818c3
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-12 08:23:22 -0700

    added lines to <h1>

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Zak Abad</title>
+</head>
+	<body>
+		<h1>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks.  </h1>
 
+	</body>
 </html>
