17aa7fd27d08bd9fb3c9501d47defc20a4caace1
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-12 08:30:47 -0700

    added lines to <h1>

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 	<title>Zak Abad</title>
 </head>
 	<body>
-		<h1>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks.  </h1>
+		<h1> Name: Zak Abad</h1>
+		<!-- Paragraph 1-->
+		<h2>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h2>
+		<!-- Paragraph 2-->
+		<h3> </h3>
 
 	</body>
 </html>
