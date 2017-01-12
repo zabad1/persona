8a3d5ec403aabb0ba46c912ef3a05dc27c40e06f
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-12 09:17:03 -0700

    Added breaks and image to persona

--- index.php
+++ index.php
@@ -4,19 +4,20 @@
 	<title>Zak Abad Persona</title>
 </head>
 	<body>
-		<h1> Name: Zak Abad</h1>
+		<h2> Name: Zak Abad</h2>
 		<!-- Paragraph 1-->
-		<h2>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h2>
+		<h3>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h3>
 		<!-- Paragraph 2-->
 		<h3>A thing you can do on IMDB is add to the content of the film. So you as a user can add to the triva. If you find imformation or spot something in a movie that isn't on the site, you can add to it. But you must have a IMDB account to do so. </h3>
 		<!-- Paragraph 3-->
-		<h4>Here is two examples of different movies</h4>
+		<h3>Here is two examples of different movies, one being a highly popular movie that has a lot of triva and the second example having very little triva added to it.</h3>
 		<!-- Links to movie triva-->
 				<a href="http://www.imdb.com/title/tt1392190/trivia?ref_=tt_ql_2"> Mad Max: Fury Road</a>
 				<br>
 				<a href="http://www.imdb.com/title/tt0015532/trivia?ref_=tt_ql_2"> The Adventures of Prince Achmed</a>
-		<!--Paragraph 4>-->
-		<h5> Now these are two different examples of movies, one being a highly popular </h5>
-
+		<br>
+		<br>
+		<br>
+		<img src="Images/IMG_5040.JPG" alt="Drinking my Costco size wine bottle" style="width: 350px; height: 500px">
 	</body>
 </html>
