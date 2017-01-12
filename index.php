870bc83acc34bda97d88b8d1692a21e21d41420c
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-12 08:56:24 -0700

    Added href links, comments, and h5 line

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Zak Abad</title>
+	<title>Zak Abad Persona</title>
 </head>
 	<body>
 		<h1> Name: Zak Abad</h1>
 		<!-- Paragraph 1-->
 		<h2>As person that is very fond of cinema. I enjoy going on to IMDB to look up films and resumes. Things that I enjoy that I look up is qoutes, triva, filming locations, and soundtracks. For any person that is slightly interested in a certain show, movie, or person. IMDB is a great site to find that infromation. </h2>
 		<!-- Paragraph 2-->
-		<h3> </h3>
+		<h3>A thing you can do on IMDB is add to the content of the film. So you as a user can add to the triva. If you find imformation or spot something in a movie that isn't on the site, you can add to it. But you must have a IMDB account to do so. </h3>
+		<!-- Paragraph 3-->
+		<h4>Here is two examples of different movies</h4>
+		<!-- Links to movie triva-->
+				<a href="http://www.imdb.com/title/tt1392190/trivia?ref_=tt_ql_2"> Mad Max: Fury Road</a>
+				<br>
+				<a href="http://www.imdb.com/title/tt0015532/trivia?ref_=tt_ql_2"> The Adventures of Prince Achmed</a>
+		<!--Paragraph 4>-->
+		<h5> Now these are two different examples of movies, one being a highly popular </h5>
 
 	</body>
 </html>
