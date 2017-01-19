2660c0ea1b45ee3d9b122281317f1e80b04a39ab
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-19 13:57:54 -0700

    corrected object variables

--- classes/Trivia.php
+++ classes/Trivia.php
@@ -1,14 +1,33 @@
 <?php
-
-class trivia
-		private $triviaId;
-		/**
-		 * id for trivia; this is the primary key
-		 * int $triviaId
-		 **/
-		private $movieId;
-		/**
-		 * id for movie; this is the foreign key
-		 * int $movieId
-		 **/
-
+/**
+ * A section of IMDB lets you see trivia for that individual movie title
+ *
+ * This bit of trivia is for the individual movie on IMDB.
+ *
+ *@author Zak Abad <zabad1@cnm.edu>
+ */
+class Trivia {
+	/**
+	 * id for trivia; this is the primary key
+	 * int $triviaId
+	 **/
+	private $triviaId;
+	/**
+	 * id for movie; this is the foreign key
+	 * int $movieId
+	 **/
+	private $movieId;
+	/**
+	 * written content that user can share or write for public
+	* */
+	private $trivial;
+	/**
+	 * movie title tied to individual trivia to share
+	 **/
+	private $movieTitle;
+/**
+ * constructor for the trivia
+ *
+ * @param int
+ */
+}
