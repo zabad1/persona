954041c3d5ab54032a2ec0d0ff080b98557c44ac
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-19 09:47:14 -0700

    finished mysql tables

--- imdb.sql
+++ imdb.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS triva;
 DROP TABLE IF EXISTS movie;
 DROP TABLE IF EXISTS profile;
 
---enitity profile--
+-- enitity profile--
 
 CREATE TABLE profile (
 		profileId INT AUTO_INCREMENT NOT NULL,
@@ -13,24 +13,33 @@ CREATE TABLE profile (
 		PRIMARY KEY(profileId)
 );
 
---enitity movie--
+-- enitity movie
 
 CREATE TABLE movie (
-		quotes VARCHAR(1000)  NOT NULL,
-		awards VARCHAR(1000)  NOT NULL,
-		userReviewDate DATETIME NOT NULL,
-		userReview VARCHAR(10000) NOT NULL,
-		synopsis VARCHAR(140) NOT NULL,
-		title VARCHAR(140) NOT NULL,
-		castCrew VARCHAR(10000) NOT NULL,
-		INDEX(movieId),
-		FOREIGN KEY(authorProfileId) REFERENCES profile(profileId),
-		PRIMARY KEY(movieId
-
+	movieId         INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	authorProfileId INT UNSIGNED                NOT NULL,
+	quotes          VARCHAR(1000)               NOT NULL,
+	awards          VARCHAR(1000)               NOT NULL,
+	userReviewDate  DATETIME                    NOT NULL,
+	userReview      VARCHAR(10000)              NOT NULL,
+	synopsis        VARCHAR(140)                NOT NULL,
+	title           VARCHAR(140)                NOT NULL,
+	castCrew        VARCHAR(10000)              NOT NULL,
+	INDEX (movieId),
+	FOREIGN KEY (authorProfileId) REFERENCES profile (profileId),
+	PRIMARY KEY (movieId)
 );
 
---enitity triva--
+-- enitity triva--
 
 CREATE TABLE triva (
+		trivaId INT UNSIGNED NOT NULL,
+		movieId INT UNSIGNED NOT NULL,
+		movieTitle VARCHAR(5000) NOT NULL,
+		trival VARCHAR(5000) NOT NULL,
+		INDEX (trivaId),
+		INDEX (movieId),
+		FOREIGN KEY (movieId) REFERENCES profile(profileId),
+		PRIMARY Key (trivaId)
 
-)
+);
