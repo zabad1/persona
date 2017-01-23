4df3892ff25557e7ca1fa400bb4d8985cbd94bba
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-23 15:44:04 -0700

    restored Trivia

--- classes/Trivia.php
+++ classes/Trivia.php
@@ -2,32 +2,302 @@
 /**
  * A section of IMDB lets you see trivia for that individual movie title
  *
- * This bit of trivia is for the individual movie on IMDB.
+ * The bits of imformation of trivia is for IMDB users to accesss and send via
+ * social media on facebook.
  *
  *@author Zak Abad <zabad1@cnm.edu>
  */
 class Trivia {
 	/**
 	 * id for trivia; this is the primary key
-	 * int $triviaId
+	 * @var int $triviaId
 	 **/
 	private $triviaId;
 	/**
 	 * id for movie; this is the foreign key
-	 * int $movieId
+	 * @var int $movieId
 	 **/
 	private $movieId;
 	/**
 	 * written content that user can share or write for public
-	* */
+	 * */
 	private $trivial;
 	/**
 	 * movie title tied to individual trivia to share
 	 **/
 	private $movieTitle;
-/**
- * constructor for the trivia
- *
- * @param int
- */
+
+	/**
+	 * constructor for the trivia
+	 *
+	 * @param int|null $triviaId id of this trivia or null if new trivia
+	 * @param int $newmovieId id of the Profile that sent this movie
+	 * @param string $newtrivial string containing actual trivial data
+	 * @param \Datetime|string|null $newmovietitle date is sent or null if set
+	 * @throws \InvalidArgumentException if data types are not vaild
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+
+class Movie {
+	/**
+	 * id for movie; this is the primary key
+	 * @var int $triviaId
+	 **/
+	private $movieId;
+	/**
+	 * id for movie; this is the foreign key
+	 * @var int $movieId
+	 **/
+	private $authorProfileId;
+	/**
+	 * written content that user can share or write for public
+	 * */
+	private $quotes;
+	/**
+	 * awards entered in by production studio
+	 **/
+	private $awards;
+	/**
+	 * brief summeries of movies writen by any user with account
+	 */
+	private $synopsis
+	/**
+	 * title given by studio account holder
+	 */
+	private $title
+	/**
+	 * castCrew inputed by studio account holder
+	 */
+	/**
+	 * constructor for the trivia
+	 *
+	 * @param int|null $movieId id of this movie or null if new movie id
+	 * @param int $authorProfileId id of the Profile that sent this movie
+	 * @param string $quotes string containing actual quotes data
+	 * @param \Datetime|string|null $awards date is sent or null if set
+	 * @param int| null $synopsis for the movie
+	 * @param not null $title for movie
+	 * @param int string $castCrew actual for data
+	 * @throws \InvalidArgumentException if data types are not vaild
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+
+	public function getmovieId() {
+		return ($this->movieId);
+
+	}
+
+	/**
+	 * mutator method for movie id
+	 *
+	 * @param int|null $newMovieId new value of movie id
+	 * @throws \RangeException if $newMovieId is not positive
+	 * @throws \TypeError if $newMovieId is not an integer
+	 *
+	 **/
+	public function setMovieId(int $newMovieId = null) {
+		//base case: if the movie id is null, this is new movie without a mySQL assigned
+		if($newMovieId === null) {
+			$this->movieId = null;
+			return;
+		}
+		//verify the movie id is positive
+		if($newMovieId <= 0) {
+			throw(new \RangeException("movie id is not positive"));
+		}
+
+		//convert and store the movie id
+		this->$newMovieId;
+	}
+	/**
+	 * accessor method for trivia id
+	 *
+	 * @return int value of trivia id
+	 **/
+	/**
+	 * @return int
+	 */
+	public function getTriviaId(): int {
+		return($this->triviaId);
+	}
+
+	/**
+	 *mutator method for tweet profile id
+	 *
+	 * @param int $newTriviaId new value of trivia id
+	 * @throws \RangeExceptionif $newTriviaId is not positive
+	 * @throws \TypeError if $newTriviaId is not an integer
+	 **/
+
+	public function setMovieTitle(int $newMovieTitle = null){
+		//base case: if movie title is null, this a new movie title without a mySQL assigned id (yet)
+		if($newMovieTitle === null){
+			throw(new \RangeException("movie title is not positive"));
+		}
+
+		//convert and store the movie title
+		$this->movieTitle = $newMovieTitle;
+	}
+
+	/**
+	 * accessor method for movie title
+	 *
+	 * @return int value of movie title
+	 **/
+	/**
+	 * @return mixed
+	 */
+	public function getMovieTitle() {
+		return ($this->movieTitle);
+	}
+
+	/**
+	 * mutator method for movie title
+	 *
+	 * @param int $newMovieTitle new value of movie title
+	 * @throws \RangeException if $newMovieTitle is not positive
+	 * @throws \TypeError if $newMovieTitle is not an integer
+	 **/
+
+	public function setTrivial(int $trivial = null)
+		//base case: if the trivial is null, this a new trivial without a mySQL assigned (yet)
+		if($newTrivial === null) {
+			$this->trivial = null;
+			return;
+		}
+
+		//verify the trivial is positive
+		if($newTrivial <= 0) {
+				throw (new \RangeException("trivial is not positive"));
+	}
+
+	//convert and store the trivial
+	$this->trivial = $newTrivial;
 }
+/**
+ * accessor method for trivial method
+ *
+ *@return int value of trivial
+ **/
+public function getTrival() {
+	return($this->trivial);
+
+}
+
+/**
+ * mutator method for trivial
+ *
+ * @param int $newTrivial new value of trivial
+ * @throws \RangeException if $newTrivial is not positive
+ * @throws \TypeError if $newTrivial is not an integer
+ **/
+
+	public function getawards() {
+		return ($this->awards);
+
+	}
+
+	/**
+	 * mutator method for movie id
+	 *
+	 * @param int|null $newawards new value of awards
+	 * @throws \RangeException if $newawards is not positive
+	 * @throws \TypeError if $newawards is not an integer
+	 *
+	 **/
+	public function setsynopsis(int $newsynopsis = null) {
+		//base case: if the synopsis is null, this is new synopsis without a mySQL assigned
+		if($newsynopsis === null) {
+			$this->synopsis = null;
+			return;
+		}
+		//verify the synopsis is positive
+		if($newsynopsis <= 0) {
+			throw(new \RangeException("synopsis is not positive"));
+		}
+
+		//convert and store the synopsis
+		this->$newsynopsis;
+	}
+	/**
+	 * accessor method for synopsis
+	 *
+	 * @return int value of synopsis
+	 **/
+
+	public function getsynopsis(): int {
+		return($this->synopsis);
+	}
+
+	/**
+	 *mutator method for synopsis profile id
+	 *
+	 * @param int $newsynopsis new value of synopsis
+	 * @throws \RangeExceptionif $newsynopsis is not positive
+	 * @throws \TypeError if $newsynopsis is not an integer
+	 **/
+
+	public function settitle(int $newtitle = null){
+		//base case: if title is null, this a new title without a mySQL assigned id (yet)
+		if($newtitle === null){
+			throw(new \RangeException("title is not positive"));
+		}
+
+		//convert and store the movie title
+		$this->title = $newtitle;
+	}
+
+	/**
+	 * accessor method for title
+	 *
+	 * @return int value of title
+	 **/
+
+	public function gettitle() {
+		return ($this->title);
+	}
+
+	/**
+	 * mutator method for title
+	 *
+	 * @param int $newtitle new value of movie title
+	 * @throws \RangeException if $newtitle is not positive
+	 * @throws \TypeError if $newtitle is not an integer
+	 **/
+
+	public function setcastCrew(int $castCrew = null)
+		//base case: if the castCrew is null, this a new castCrew without a mySQL assigned (yet)
+		if($newcastCrew === null) {
+			$this->castCrew = null;
+			return;
+		}
+
+		//verify the castCrew is positive
+		if($newcastCrew <= 0) {
+				throw (new \RangeException("castCrew is not positive"));
+	}
+
+	//convert and store the castCrew
+	$this->castCrew = $newcastCrew;
+}
+/**
+ * accessor method for castCrew method
+ *
+ *@return int value of castCrew
+ **/
+public function getcastCrew() {
+	return($this->castCrew);
+
+}
+
+/**
+ * mutator method for castCrew
+ *
+ * @param int $newcastCrew new value of castCrew
+ * @throws \RangeException if $newcastCrew is not positive
+ * @throws \TypeError if $newcastCrew is not an integer
+ **/
--- index.php
+++ index.php
@@ -4,65 +4,48 @@
 	<title>Zak Abad Persona</title>
 </head>
 	<body>
-		<!-- css -->
-		.rotateimg90 {
-			-webkit-transform:rotate(90deg);
-			-moz-transform: rotate(90deg);
-			-ms-transform: rotate(90deg);
-			-o-transform: rotate(90deg);
-			transform:rotate(90deg);
-		}
-			<br>
-			<img src="Images/IMG_2033.JPG" alt="Pie eating" style="float:right;width:250px; height:350px" class="rotate90">
-			<img src="Images/IMG_5040.JPG" alt="Drinking my Costco size wine bottle" style="float:right;width:250px; height:350px">
-			<ul style="list-style-type:disc">
-				<li>Name:Clifford Wood-Buck a.k.a. Sponge</li>
-				<li>Age:132.9 Mercurian Years (that's according to Mercury)</li>
-				<li>Occupation:Raging Wino & Pie Eating Contestant Winner</li>
-				<li>Needs:Sponge has made a full-time living as a pie eating contestant, so he can fuel his wino drinking hobby through the week. Losing one contest can reduce the amount of cheap wine he needs for the next weekend contest. If that happens, Clifford listens to Katy Perry music the whole week to motivate him to win the next contest.
-				</li>
-				<li>Goals: Other then drinking gallons of cheap wine during the week and scraffing pies down for income. Sponge likes to get on IMDB and write incorrect information about movies, especially in the triva section. </li>
+		<h1>Persona</h1>
+		<h4>Cliff is 36 years of age. He doesn't use technology all that much. But sometimes. When he does so he accesses it through his desktop windows computer. He enjoys kicking back on the couch watching old movies and television. He enjoys going on to IMDB and looking up info for different movies and television he watches.
+			<h1>Use Goal</h1>
+		<h4>Cliff will be able to get on IMDB and look up information on movies and television.</h4>
+			<h1>Interaction Flow</h1>
+			<h4>
+				1.Cliff gets on computer/opens browser.
 				<br>
-			</ul>
-			<br>
-		<!-- Persona-->
-		<h3> </h3>
-		<!-- Case-->
-		<h3>.</h3>
-		<!--Examples of bad triva-->
-		<h3></h3>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<!-- Links to movie triva-->
-		<img src="Images/madmaxfuryroad.jpg" width="450"; height="250";>
-		<br>
-		<a href="http://www.imdb.com/title/tt1392190/trivia?ref_=tt_ql_2"> Mad Max: Fury Road</a>
-		<br>
-		<br>
-		<br>
-		<img src="Images/theAdventuresofPrinceAchmed.jpg" width="450"; height="250">
-		<br>
-		<a href="http://www.imdb.com/title/tt0015532/trivia?ref_=tt_ql_2"> The Adventures of Prince Achmed</a>
-		<br>
-		<br>
-		<br>
-		<!-- Interaction flow-->
-		<ol>1)Turns on comupter
-			<br>
-			<br>
-			3)Gets in the server and becomes the main host
-		</ol>
-		<table>
-			<p><h1>Profile</h1></p>
-			<th>
-
-			</th>
-		</table>
+				2.He then searches for IMDB on computer.
+				<br>
+				3.Clicks on IMDB
+				<br>
+				4.Seraches for movie on IMDB serach bar up top.
+				<br>
+				5.Looks into the movie info on IMDB
+			</h4>
+			<h1>Conceptual Model</h1>
+			<h2>Entities & Attributes
+				<h3>Profile</h3>
+				<ul>
+					<li>profileEmail</li>
+					<li>profileId</li>
+					<li>username</li>
+				</ul>
+				<h3>Movie</h3>
+				<ul>
+					<li>authorProfileId</li>
+					<li>movieId</li>
+					<li>quotes</li>
+					<li>awards</li>
+					<li>synopsis</li>
+					<li>title</li>
+					<li>castCrew</li>
+				</ul>
+				<h3>Trivia</h3>
+				<ul>
+					<li>triviaId</li>
+					<li>movieId</li>
+					<li>trivial</li>
+					<li>movieTitle</li>
+				</ul>
+				<h3>Relations</h3>
+				<img src="Images/IMDB-%20Zak%20Abad%20persona%252FERD%20(1).png">
 	</body>
 </html>
